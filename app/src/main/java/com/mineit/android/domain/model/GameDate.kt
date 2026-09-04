5e8fc5c09aefb607df6617ea60a9c725cfc04b0c
kevvy555 <kevvy555@googlemail.com>
2026-09-04 08:43:24 +0100

    domain: add canonical game date value

---
+++ app/src/main/java/com/mineit/android/domain/model/GameDate.kt
@@ -0,0 +1,28 @@
+package com.mineit.android.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GameDate(
+    val year: Int,
+    val day: Int,
+) {
+    init {
+        require(year >= 1) { "Game year must be at least 1." }
+        require(day in 1..DAYS_PER_YEAR) { "Game day must be between 1 and $DAYS_PER_YEAR." }
+    }
+
+    fun toAbsoluteDay(): AbsoluteDay = AbsoluteDay((year - 1) * DAYS_PER_YEAR + day)
+
+    companion object {
+        const val DAYS_PER_YEAR = 365
+
+        fun fromAbsoluteDay(absoluteDay: AbsoluteDay): GameDate {
+            val zeroBased = absoluteDay.value - 1
+            return GameDate(
+                year = zeroBased / DAYS_PER_YEAR + 1,
+                day = zeroBased % DAYS_PER_YEAR + 1,
+            )
+        }
+    }
+}
