3cca4fe6accab63ac25c76f82e92de60931693a8
kevvy555 <kevvy555@googlemail.com>
2026-09-04 11:41:37 +0100

    feat: route native app through main menu

--- app/src/main/java/com/mineit/android/ui/MineItApp.kt
+++ app/src/main/java/com/mineit/android/ui/MineItApp.kt
@@ -10,43 +10,56 @@ import com.mineit.android.ui.theme.MineItTheme
 @Composable
 fun MineItApp(viewModel: GameViewModel = viewModel()) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val screen by viewModel.screen.collectAsStateWithLifecycle()
+    val entryReady by viewModel.entryReady.collectAsStateWithLifecycle()
+    val canContinue by viewModel.canContinue.collectAsStateWithLifecycle()
     val metrics by viewModel.metrics.collectAsStateWithLifecycle()
     val network by viewModel.network.collectAsStateWithLifecycle()
     val spaceport by viewModel.spaceport.collectAsStateWithLifecycle()
     val simulationSpeed by viewModel.simulationSpeed.collectAsStateWithLifecycle()
     val selectedCoordinate by viewModel.selectedSector.collectAsStateWithLifecycle()
     val statusMessage by viewModel.statusMessage.collectAsStateWithLifecycle()
-    val selectedSector = selectedCoordinate?.let { state.activeColony.world.tileAt(it) }
-    val selectedSurveyDays = selectedCoordinate?.let(viewModel::surveyDays)
 
     MineItTheme {
-        MineItScreen(
-            state = state,
-            metrics = metrics,
-            network = network,
-            spaceport = spaceport,
-            simulationSpeed = simulationSpeed,
-            selectedSector = selectedSector,
-            selectedCoordinate = selectedCoordinate,
-            selectedSurveyDays = selectedSurveyDays,
-            statusMessage = statusMessage,
-            powerPreview = viewModel.buildingPreview(DevelopmentKind.POWER),
-            housingPreview = viewModel.buildingPreview(DevelopmentKind.HOUSING),
-            industryPreview = viewModel.buildingPreview(DevelopmentKind.INDUSTRY),
-            headquartersPreview = viewModel.buildingPreview(DevelopmentKind.HEADQUARTERS),
-            extractionPreview = viewModel.extractionPreview(),
-            upgradePreview = viewModel.upgradePreview(),
-            departureGate = viewModel.departureGate(),
-            onSelectLandingSite = viewModel::selectLandingSite,
-            onSelectSector = viewModel::selectSector,
-            onSurveySelectedSector = viewModel::surveySelectedSector,
-            onBuild = viewModel::buildSelected,
-            onDevelopExtraction = viewModel::developSelectedResource,
-            onUpgrade = viewModel::upgradeSelected,
-            onDemolish = viewModel::demolishSelected,
-            onSetPrimaryHeadquarters = viewModel::setSelectedAsPrimaryHeadquarters,
-            onAdvanceDay = viewModel::advanceDay,
-            onSetSimulationSpeed = viewModel::setSimulationSpeed,
-        )
+        if (screen == GameScreen.MAIN_MENU) {
+            MainMenuScreen(
+                ready = entryReady,
+                canContinue = canContinue,
+                statusMessage = statusMessage,
+                onContinue = viewModel::continueGame,
+                onNewGame = viewModel::startNewGame,
+            )
+        } else {
+            val selectedSector = selectedCoordinate?.let { state.activeColony.world.tileAt(it) }
+            val selectedSurveyDays = selectedCoordinate?.let(viewModel::surveyDays)
+            MineItScreen(
+                state = state,
+                metrics = metrics,
+                network = network,
+                spaceport = spaceport,
+                simulationSpeed = simulationSpeed,
+                selectedSector = selectedSector,
+                selectedCoordinate = selectedCoordinate,
+                selectedSurveyDays = selectedSurveyDays,
+                statusMessage = statusMessage,
+                powerPreview = viewModel.buildingPreview(DevelopmentKind.POWER),
+                housingPreview = viewModel.buildingPreview(DevelopmentKind.HOUSING),
+                industryPreview = viewModel.buildingPreview(DevelopmentKind.INDUSTRY),
+                headquartersPreview = viewModel.buildingPreview(DevelopmentKind.HEADQUARTERS),
+                extractionPreview = viewModel.extractionPreview(),
+                upgradePreview = viewModel.upgradePreview(),
+                departureGate = viewModel.departureGate(),
+                onSelectLandingSite = viewModel::selectLandingSite,
+                onSelectSector = viewModel::selectSector,
+                onSurveySelectedSector = viewModel::surveySelectedSector,
+                onBuild = viewModel::buildSelected,
+                onDevelopExtraction = viewModel::developSelectedResource,
+                onUpgrade = viewModel::upgradeSelected,
+                onDemolish = viewModel::demolishSelected,
+                onSetPrimaryHeadquarters = viewModel::setSelectedAsPrimaryHeadquarters,
+                onAdvanceDay = viewModel::advanceDay,
+                onSetSimulationSpeed = viewModel::setSimulationSpeed,
+            )
+        }
     }
 }
