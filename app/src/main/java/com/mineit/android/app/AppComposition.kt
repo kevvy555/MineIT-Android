e7680bc5c846dee7f86e072f4b594ea2eaccaba9
kevvy555 <kevvy555@googlemail.com>
2026-09-04 17:04:27 +0100

    app: compose Phase 6 commercial domain owners

--- app/src/main/java/com/mineit/android/app/AppComposition.kt
+++ app/src/main/java/com/mineit/android/app/AppComposition.kt
@@ -3,14 +3,21 @@ package com.mineit.android.app
 import android.app.Application
 import com.mineit.android.BuildConfig
 import com.mineit.android.data.save.FileGameStatePersistence
+import com.mineit.android.domain.buyers.BuyerService
 import com.mineit.android.domain.colony.ColonyDevelopmentService
 import com.mineit.android.domain.colony.ColonyNetworkService
 import com.mineit.android.domain.colony.HeadquartersService
 import com.mineit.android.domain.colony.SpaceportService
+import com.mineit.android.domain.commercial.CommercialDayService
+import com.mineit.android.domain.contracts.ContractService
+import com.mineit.android.domain.events.CorporateEventService
+import com.mineit.android.domain.logging.GameLogService
 import com.mineit.android.domain.model.ColonyId
 import com.mineit.android.domain.model.GameState
 import com.mineit.android.domain.model.NewGameFactory
+import com.mineit.android.domain.reputation.ReputationService
 import com.mineit.android.domain.simulation.DailySimulationEngine
+import com.mineit.android.domain.trade.CorporateTradeService
 import com.mineit.android.domain.world.SurveyGameService
 import java.io.File
 
@@ -22,10 +29,24 @@ class AppComposition(application: Application) {
     val colonyNetworkService = ColonyNetworkService(headquartersService)
     val colonyDevelopmentService = ColonyDevelopmentService(headquartersService)
     val spaceportService = SpaceportService()
+    val reputationService = ReputationService()
+    val corporateTradeService = CorporateTradeService(reputationService)
+    val contractService = ContractService(reputationService)
+    val buyerService = BuyerService(reputationService)
+    val corporateEventService = CorporateEventService()
+    val gameLogService = GameLogService()
     val dailySimulationEngine = DailySimulationEngine(
         surveyGameService = surveyGameService,
         networkService = colonyNetworkService,
         headquartersService = headquartersService,
+    )
+    val commercialDayService = CommercialDayService(
+        dailySimulationEngine = dailySimulationEngine,
+        tradeService = corporateTradeService,
+        buyerService = buyerService,
+        contractService = contractService,
+        eventService = corporateEventService,
+        gameLogService = gameLogService,
     )
 
     private val initialState = createNewGame()
