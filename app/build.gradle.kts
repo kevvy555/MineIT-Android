b302cdca652557ade620f9bc7179dfbff24438cb
kevvy555 <kevvy555@googlemail.com>
2026-09-04 08:09:50 +0100

    test: add JSON support for parity fixtures

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
 }
